e16d24c63ebf7e58a6b3dc0ba87b517eaf9fea86
Decrescent398 <ganeshhgunda123@gmail.com>
2025-03-26 15:45:25 +0530

    Restructuring

--- src/subprocesses/scripts/data_scraper.py
+++ src/subprocesses/scripts/data_scraper.py
@@ -12,7 +12,7 @@ def celestrak_data():
     except: 
         pass
     
-    database = pathlib.Path('./subprocesses/databases')
+    database = pathlib.Path('src/subprocesses/databases')
 
     space_stations = 'stations.csv'
     stations_url = base + '?GROUP=stations&FORMAT=csv'
