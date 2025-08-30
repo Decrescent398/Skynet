abffe36da868b7df7bdc29d9970da4aefd2abd90
Decrescent398 <ganeshhgunda123@gmail.com>
2025-08-30 15:05:25 +0530

    Renamed reflex dir due to conflict with root dir

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Skynet",
+    app_name="skynet-web",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
