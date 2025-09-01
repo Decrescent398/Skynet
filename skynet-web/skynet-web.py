39bc1ffc57d61ae0d832dbae43bb1f25be882896
Decrescent398 <ganeshhgunda123@gmail.com>
2025-09-01 15:31:47 +0530

    Added Space Stations

--- skynet-web/skynet-web.py
+++ skynet-web/skynet-web.py
@@ -15,7 +15,6 @@ class State(rx.State):
     """The app state."""
     df: pd.DataFrame
     objects: list = []
-    coord: list = []
     fig: go.Figure = px.scatter_geo(
         pd.DataFrame(columns=["lat", "lon"]),
         lat="lat",
@@ -33,15 +32,18 @@ class State(rx.State):
         
         coords = []
         
-        for sat in self.objects:
+        for idx, classification in enumerate(self.objects):
+            typ = "Stations" if idx == 1 else "Satellite"
+            for sat in classification:
             
-            lat, lon = wgs84.latlon_of(sat.at(time))
-            coords.append((lat.degrees, lon.degrees))
+                lat, lon = wgs84.latlon_of(sat.at(time))
+                coords.append((lat.degrees, lon.degrees, typ))
             
-        self.df = pd.DataFrame(coords, columns=["lat", "lon"])
+        self.df = pd.DataFrame(coords, columns=["lat", "lon", "type"])
         self.fig = px.scatter_geo(self.df,
                         lat="lat",
                         lon="lon",
+                        color="type",
                         projection="orthographic", 
                         template="plotly_dark",
                         width=1300,
@@ -64,7 +66,7 @@ class State(rx.State):
             load.download(url, filename = str(file))
     
     @rx.event
-    def download_celestrak_data(self, filename='databases/active_satellites.csv'):
+    def download_celestrak_data(self):
         # Base URL for Celestrak NORAD elements in CSV format
         base = 'https://celestrak.org/NORAD/elements/gp.php'
 
@@ -82,11 +84,16 @@ class State(rx.State):
         self.download_limiter(stations_filepath, stations_url)
         self.download_limiter(sats_filepath, active_sats_url)
         
-        with load.open(filename, mode='r') as file:
+        with load.open(sats_filepath.as_posix(), mode='r') as file:
             
-            data = list(csv.DictReader(file))
+            sats_data = list(csv.DictReader(file))
             
-        self.objects = [EarthSatellite.from_omm(timescale, fields) for fields in data]
+        with load.open(stations_filepath.as_posix(), mode='r') as file:
+            
+            stations_data = list(csv.DictReader(file))
+            
+        self.objects.append([EarthSatellite.from_omm(timescale, fields) for fields in sats_data])
+        self.objects.append([EarthSatellite.from_omm(timescale, fields) for fields in stations_data])
         
 
 timescale = load.timescale()
@@ -96,16 +103,17 @@ def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="bottom-left"),
-        rx.box(
-            rx.plotly(data=State.fig,
-                      on_after_plot=State.create_map,
-                      use_resize_handler=True,
-                      config={"displayModeBar":False}),
-            on_mount=State.download_celestrak_data,
-            align = "center",
-            justify = "center",
-            display = "flex",
-            
+        rx.hstack(
+            rx.box(
+                rx.plotly(data=State.fig,
+                        on_after_plot=State.create_map,
+                        use_resize_handler=True,
+                        config={"displayModeBar":False}),
+                on_mount=State.download_celestrak_data,
+                align = "center",
+                justify = "center",
+                display = "flex",
+            )
         ),
     )
 
